263fefeba76747cb00b4329148b6cf8675c4e9bb
Josh Cai <yuanshi419@gmail.com>
2014-09-15 21:36:23 -0500

    added projects section

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -3,26 +3,31 @@ var website = angular.module('website', []);
 website.controller('mainCtrl', function ($scope) {
   $scope.includes = [
     'html/about.html',
-    'html/projects.html',
-    'html/contact.html'
+    'html/contact.html',
+    'html/projects.html'
   ];
 
   $scope.projects = [
     {
       'name': 'PleaseObserve.me',
       'link': 'http://pleaseobserve.me',
-      'desc': 'hello'
+      'desc': 'Created a web app to keep track of what I did during the day for around 75 days'
     },
     {
       'name': 'LectureSync',
       'link': 'http://lecture-sync.com',
-      'desc': 'hello'
+      'desc': 'Built at HackTech with friends - lets you sync slides with a professor\'s lecture and take notes on them'
     },
     {
       'name': 'UTD JavaScript Camp',
       'link': 'http://utdcs.herokuapp.com',
-      'desc': 'hello'
+      'desc': 'Volunteered at a camp to teach high school kids how to code - they uploaded their JavaScript code here'
     },
   ];
 
 });
+
+function ToggleProjects() {
+  var p = document.getElementById("projects");
+  p.classList.toggle("hidden");
+}
